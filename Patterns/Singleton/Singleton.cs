cbd8c998968af6526de6350ccf687bb28e6c8bca
YurritAvonds <yurrit.avonds@gmail.com>
2026-01-31 12:00:15 +0100

    Migrated Singleton from old project

---
+++ Patterns/Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿namespace Patterns.Singleton;
+
+public class Singleton
+{
+	public int FirstProperty { get; set; }
+
+	private static Singleton? instance;
+
+	private Singleton()
+	{
+		FirstProperty = 0;
+	}
+
+	public static Singleton GetInstance()
+	{
+		instance ??= new Singleton();
+
+		return instance;
+	}
+}
---
+++ UnitTests/Singleton/SingletonTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Patterns.Singleton;
+
+namespace UnitTests.Composite;
+
+[TestFixture]
+public class SingletonTests
+{
+    [Test]
+    public void FirstMethod_ReturnsFirstProperty()
+    {
+        // Arrange
+        Singleton.GetInstance().FirstProperty = 42;
+
+        // Act
+        Singleton.GetInstance().FirstProperty = 50;
+
+        // Assert
+        Assert.Equals(50, Singleton.GetInstance().FirstProperty);
+    }
+}
