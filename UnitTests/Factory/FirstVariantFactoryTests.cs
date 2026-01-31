50b26ceacc93f53f0b1b92657dbe2840d16d500f
YurritAvonds <yurrit.avonds@gmail.com>
2026-01-31 12:41:37 +0100

    Added tests for factory

---
+++ UnitTests/Factory/FirstVariantFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Patterns.Factory;
+
+namespace UnitTests.Factory
+{
+    public class FirstVariantFactoryTests
+    {
+        [Test]
+        public void SharedMethod_AllVariants_ReturnsExpectedResults()
+        {
+            // Arrange
+
+            // Act
+            var factory = new FirstVariantFactory();
+            
+            // Assert
+            var expected = new[] { false, false, true };
+            var i = 0;
+            foreach (IBaseType variant in factory)
+            {
+                variant.SharedMethod().Should().Be(expected[i]);
+                i++;
+            }
+        }
+    }
+}
---
+++ UnitTests/Factory/SecondVariantFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Patterns.Factory;
+
+namespace UnitTests.Factory
+{
+    public class SecondVariantFactoryTests
+    {
+        [Test]
+        public void SharedMethod_AllVariants_ReturnsExpectedResults()
+        {
+            // Arrange
+            var factory = new SecondVariantFactory();
+
+            // Act
+
+            // Assert
+            var expected = new[] { false, false, true };
+            var i = 0;
+            foreach (IBaseType variant in factory)
+            {
+                variant.SharedMethod().Should().Be(expected[i]);
+                i++;
+            }
+        }
+    }
+}
