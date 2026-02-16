b709556f7f6ab184ba9cc3c27bb976bf02b31bef
YurritAvonds <yurrit.avonds@gmail.com>
2026-02-16 20:35:52 +0100

    Added Memento pattern

---
+++ Patterns/Standard/Memento/IOriginator.cs
@@ -0,0 +1,9 @@
+﻿namespace Patterns.Standard.Memento;
+
+public interface IOriginator
+{
+    Originator.Memento CreateMemento();
+    void RestoreMemento(Originator.Memento memento);
+    void SetInteger(int newInteger);
+    void SetString(string newString);
+}
---
+++ UnitTests/Standard/MementoTests.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using Patterns.Standard.Memento;
+
+namespace UnitTests.Standard;
+
+internal class MementoTests
+{
+    [Test]
+    public void UpdateState()
+    {
+        // Arrange
+        var originator = new Originator();
+        var client = new Client(originator);
+
+        // Act
+        client.SetString("First state");
+        client.SetInteger(42);
+        client.SetString("Second state");
+        client.SetInteger(100);
+
+        // Assert
+        originator.StateString.Should().Be("Second state");
+        originator.StateInteger.Should().Be(100);
+    }
+
+    [Test]
+    public void UndoState()
+    {
+        // Arrange
+        var originator = new Originator();
+        var client = new Client(originator);
+
+        // Act
+        client.SetString("First state");
+        client.SetInteger(42);
+        client.SetString("Second state");
+        client.SetInteger(100);
+        client.Undo();
+
+        // Assert
+        originator.StateString.Should().Be("Second state");
+        originator.StateInteger.Should().Be(42);
+    }
+
+    [Test]
+    public void UndoMultiple()
+    {
+        // Arrange
+        var originator = new Originator();
+        var client = new Client(originator);
+
+        // Act
+        client.SetString("First state");
+        client.SetInteger(42);
+        client.SetString("Second state");
+        client.SetInteger(100);
+        client.Undo();
+        client.Undo();
+
+        // Assert
+        originator.StateString.Should().Be("First state");
+        originator.StateInteger.Should().Be(42);
+    }
+
+    [Test]
+    public void UndoAll()
+    {
+        // Arrange
+        var originator = new Originator();
+        var client = new Client(originator);
+
+        // Act
+        client.SetString("First state");
+        client.SetInteger(42);
+        client.SetString("Second state");
+        client.SetInteger(100);
+        client.Undo();
+        client.Undo();
+        client.Undo();
+        client.Undo();
+
+        // Assert
+        originator.StateString.Should().Be(string.Empty);
+        originator.StateInteger.Should().Be(0);
+    }
+
+    [Test]
+    public void UndoBeyondHistory()
+    {
+        // Arrange
+        var originator = new Originator();
+        var client = new Client(originator);
+
+        // Act
+        client.SetString("First state");
+        client.SetInteger(42);
+        client.SetString("Second state");
+        client.SetInteger(100);
+        client.Undo();
+        client.Undo();
+        client.Undo();
+        client.Undo();
+        client.Undo();
+
+        // Assert
+        originator.StateString.Should().Be(string.Empty);
+        originator.StateInteger.Should().Be(0);
+    }
+}
