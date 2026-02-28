123c72f4fd6d48a6c8f5aa219b38ec14befbc286
YurritAvonds <yurrit.avonds@gmail.com>
2026-02-28 15:57:20 +0100

    FHIR FullName test + refined how fullname is constructed

--- Patterns/Fhir/HumanName/HumanName.cs
+++ Patterns/Fhir/HumanName/HumanName.cs
@@ -1,44 +1,49 @@
 ﻿using Patterns.Fhir.ValueSet;
-using System;
-using System.Collections.Generic;
 using System.Text;
 
-namespace Patterns.Fhir.HumanName
-{
-    internal class HumanName
-    {
-        public Code? Use { get; set; }
-        public string? Text { get; set; }
-        public string? Family { get; set; }
-        public ICollection<string>? Given { get; set; }
-        public ICollection<string>? Prefix { get; set; }
-        public ICollection<string>? Suffix { get; set; }
-        public DateTime? PeriodStart { get; set; }
-        public DateTime? PeriodEnd { get; set; }
+namespace Patterns.Fhir.HumanName;
 
-        public string FullName
+public class HumanName
+{
+    public Code? Use { get; set; }
+    public string? Text { get; set; }
+    public string? Family { get; set; }
+    public ICollection<string>? Given { get; set; }
+    public ICollection<string>? Prefix { get; set; }
+    public ICollection<string>? Suffix { get; set; }
+    public DateTime? PeriodStart { get; set; }
+    public DateTime? PeriodEnd { get; set; }
+
+    public string? FullName
+    {
+        get
         {
-            get
+            if (Family == null
+                && (Given == null || !Given.Any())
+                && (Prefix == null || !Prefix.Any())
+                && (Suffix == null || !Suffix.Any()))
             {
-                var fullName = new StringBuilder();
-                if (Prefix != null && Prefix.Any())
-                {
-                    fullName.Append(string.Join(" ", Prefix) + " ");
-                }
-                if (Given != null && Given.Any())
-                {
-                    fullName.Append(string.Join(" ", Given) + " ");
-                }
-                if (!string.IsNullOrEmpty(Family))
-                {
-                    fullName.Append(Family);
-                }
-                if (Suffix != null && Suffix.Any())
-                {
-                    fullName.Append(" " + string.Join(" ", Suffix));
-                }
-                return fullName.ToString().Trim();
+                return null;
             }
+
+            var fullName = new StringBuilder();
+            if (Prefix != null && Prefix.Any())
+            {
+                fullName.Append(string.Join(" ", Prefix) + " ");
+            }
+            if (Given != null && Given.Any())
+            {
+                fullName.Append(string.Join(" ", Given) + " ");
+            }
+            if (!string.IsNullOrEmpty(Family))
+            {
+                fullName.Append(Family);
+            }
+            if (Suffix != null && Suffix.Any())
+            {
+                fullName.Append(" " + string.Join(" ", Suffix));
+            }
+            return fullName.ToString().Trim();
         }
     }
 }
---
+++ UnitTests/Fhir/HumanNameTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace UnitTests.Fhir;
+
+internal class HumanNameTests
+{
+    [Test]
+    [TestCase(new[] { "Dr.", "Mr." }, new[] { "John", "A." }, "Doe", new[] { "Jr." }, "Dr. Mr. John A. Doe Jr.")]
+    [TestCase(null, new[] { "John" }, "Doe", null, "John Doe")]
+    [TestCase(null, null, "Doe", null, "Doe")]
+    [TestCase(null, new[] { "John" }, null, null, "John")]
+    [TestCase(new[] { "Mr." }, null, null, null, "Mr.")]
+    [TestCase(null, null, null, new[] { "Jr." }, "Jr.")]
+    [TestCase(null, null, null, null, null)]
+    public void FullName_ShouldReturnCorrectFullName(string[]? prefix, string[]? given, string? family,
+        string[]? suffix, string? expectedFullName)
+    {
+        // Arrange
+        var humanName = new Patterns.Fhir.HumanName.HumanName
+        {
+            Prefix = prefix,
+            Given = given,
+            Family = family,
+            Suffix = suffix
+        };
+
+        // Act
+        var fullName = humanName.FullName;
+
+        // Assert
+        fullName.Should().Be(expectedFullName);
+    }
+}
