15506465d4d158cdde7cadef31fe6d24f3535e5f
YurritAvonds <yurrit.avonds@gmail.com>
2026-03-14 20:39:56 +0100

    Initial documentation setup

--- Patterns/Fhir/ValueSet/Code.cs
+++ Patterns/Fhir/ValueSet/Code.cs
@@ -1,5 +1,11 @@
 ﻿namespace Patterns.Fhir.ValueSet;
 
+/// <summary>
+/// Equivalent of the Coding in FHIR
+/// https://www.hl7.org/fhir/R4/datatypes.html#Coding
+/// </summary>
+/// <param name="Value"></param>
+/// <param name="Display"></param>
 public record Code(string Value, string Display)
 {
     public string Value { get; private set; } = Value;
--- Patterns/Fhir/ValueSet/ValueSet.cs
+++ Patterns/Fhir/ValueSet/ValueSet.cs
@@ -1,11 +1,23 @@
 ﻿namespace Patterns.Fhir.ValueSet;
 
+/// <summary>
+/// Equivalent of a ValueSet in FHIR
+/// https://hl7.org/fhir/R4/valueset.html
+/// </summary>
+/// <param name="uri"></param>
+/// <param name="oid"></param>
 public class ValueSet(string uri, string oid) : ISystem
 {
     public string Uri { get; private set; } = uri;
     public string Oid { get; private set; } = oid;
     public CodeSystem[] Codes { get; set; } = [];
 
+    /// <summary>
+    /// Check whether the ValueSet contains a given code from a given system.
+    /// </summary>
+    /// <param name="system">URI of the system</param>
+    /// <param name="value">Value of the code to be checked</param>
+    /// <returns></returns>
     public bool ContainsCode(string system, string value)
     {
         var codeSystem = Codes.FirstOrDefault(codeSystem
